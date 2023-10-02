caa371bcc003bd7d71535e1da848062f41180322
Michael Nguyen <mnguyen2000@gmail.com>
2023-10-01 19:44:13 -0700

    Finished Search and Activity Pages and functionality

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import Thread from "../models/thread.model";
 import { connectToDB } from "../mongoose"
+import { FilterQuery, SortOrder } from "mongoose";
 
 interface Params{
     userId: string;
@@ -81,5 +82,74 @@ export async function fetchUserPosts(userId: string) {
             return threads;
     } catch (error: any) {
         throw new Error(`Failed to fetch user posts: ${error.message}`)
+    }
+}
+
+export async function fetchUsers({
+    userId,
+    searchString = "",
+    pageNumber = 1,
+    pageSize = 20,
+    sortBy = "desc"
+} : {
+    userId: string;
+    searchString?: string;
+    pageNumber?: number;
+    pageSize?: number;
+    sortBy?: SortOrder
+}) {
+    try {
+        connectToDB();
+        const skipAmount = (pageNumber - 1) * pageSize;
+        const regex = new RegExp(searchString, "i");
+        const query: FilterQuery<typeof User> = {
+            id: { $ne: userId }
+        }
+        if(searchString.trim() !== '') {
+            query.$or = [
+                { username: { $regex: regex } },
+                { name: { $regex: regex } }
+            ]
+        }
+
+        const sortOptions = { createdAt: sortBy };
+
+        const usersQuery = User.find(query)
+            .sort(sortOptions)
+            .skip(skipAmount)
+            .limit(pageSize);
+        const totalUsersCount = await User.countDocuments(query);
+        const users = await usersQuery.exec();
+        const isNext = totalUsersCount > skipAmount + users.length;
+        return { users, isNext };
+    } catch (error: any) {
+        throw new Error(`Failed to fetch users: ${error.message}`)
+        
+    }
+}
+
+export async function getActivity(userId: string) {
+    try {
+        connectToDB();
+        // find all threads created by the User
+        const userThreads = await Thread.find({ author: userId });
+        // Collect all the child thread ids (replies or comments) from the children field
+        const childThreadIds = userThreads.reduce((acc, userThread) => {
+            return acc.concat(userThread.children)
+        }, [])
+        // find all the threads that the user has replied to
+        const replies = await Thread.find({
+            _id: { $in: childThreadIds},
+            author: { $ne: userId}
+        }).populate({
+            path: 'author',
+            model: User,
+            select: 'name image _id'
+        })
+
+        return replies;
+    } catch (error: any) {
+        throw new Error(`Failed to fetch activity: ${error.message}`);
+        
     }
 }
